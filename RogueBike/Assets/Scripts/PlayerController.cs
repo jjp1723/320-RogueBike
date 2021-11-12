c3bbf52cc99d75397c83ab7ca73667124e9e3c63
Noah Shields <noahshields5@gmail.com>
2021-11-12 14:36:42 -0500

    Added player with controls
    To edit how the player works we just need to tune the rigid body values

--- RogueBike/Assets/Scripts/PlayerController.cs
+++ RogueBike/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
 
     [SerializeField]
+    private float forwardForceMagnitude;
+    [SerializeField]
+    private float brakeForceMagnitude;
+    [SerializeField]
+    private float rotationalSpeed;
+
+    [SerializeField]
     private float MAX_VELOCITY_MAG;
     [SerializeField]
     private float MAX_ACCELERATION_MAG;
@@ -18,21 +25,43 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         player = this.gameObject;
-        rb = player.GetComponent <Rigidbody2D>();
+        rb = player.GetComponent<Rigidbody2D>();
     }
-    private void MovePlayer(Vector2 forceVec)
+
+    public void CheckForInput() {
+        Debug.Log(rb.velocity);
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotatePlayer(rotationalSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            RotatePlayer(-rotationalSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            MovePlayer(forwardForceMagnitude * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MovePlayer(brakeForceMagnitude * Time.deltaTime);
+        }
+        
+    }
+
+    private void MovePlayer(float force)
     {
-        rb.AddForce(forceVec);
+        rb.AddForce(force * player.transform.up);
         currVelocity = rb.velocity.magnitude;
 
-        if(currVelocity > MAX_VELOCITY_MAG)
+        if (currVelocity > MAX_VELOCITY_MAG)
         {
-            rb.velocity = MAX_VELOCITY_MAG * gameObject.transform.forward;
+            rb.velocity = MAX_VELOCITY_MAG * player.transform.forward;
         }
     }
 
     private void RotatePlayer(float rotation)
     {
-        player.
+        player.transform.rotation = Quaternion.Euler(player.transform.forward * rotation + player.transform.eulerAngles);
     }
 }
