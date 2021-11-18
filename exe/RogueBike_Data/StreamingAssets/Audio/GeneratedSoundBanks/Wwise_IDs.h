de26ea6970ae2b7cac9d07ba6aeea2c8a4661f35
Cassiar <cdb7951@rit.edu>
2021-11-18 17:18:20 -0500

    added exe build for project

---
+++ exe/RogueBike_Data/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID LOSE_SFX = 2894173942U;
+        static const AkUniqueID MENU = 2607556080U;
+        static const AkUniqueID RACESTART_SFX = 1518346846U;
+        static const AkUniqueID SET_STATE_END = 2300403011U;
+        static const AkUniqueID SET_STATE_GAME = 4207344442U;
+        static const AkUniqueID SET_STATE_MENU = 2302270969U;
+        static const AkUniqueID WIN_SFX = 3574552973U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATEGROUP
+        {
+            static const AkUniqueID GROUP = 2935564353U;
+
+            namespace STATE
+            {
+                static const AkUniqueID END = 529726532U;
+                static const AkUniqueID GAME = 702482391U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace STATEGROUP
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID UNITYSOUNDBANK = 4251510771U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
