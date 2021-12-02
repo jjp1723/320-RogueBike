0aa39d5e2fd06655ef8423ebf9fb9732ebb21dc8
Noah Shields <noahshields5@gmail.com>
2021-12-02 14:01:37 -0500

    Added turn right card

--- RogueBike/Assets/Scripts/GameManager.cs
+++ RogueBike/Assets/Scripts/GameManager.cs
@@ -17,30 +17,37 @@ public class GameManager : MonoBehaviour
     private List<GameObject> cards = new List<GameObject>();
     private Dictionary<string, TurnCard> turnDict = new Dictionary<string, TurnCard>();
 
-    private List<GameObject> shownCards = new List<GameObject>();
+    //private List<GameObject> shownCards = new List<GameObject>();
 
     //[SerializeField]
     //private Camera camera;
 
-    [SerializeField]
-    private GameObject leftTurnCard_TEMP;
+    //[SerializeField]
+    //private GameObject leftTurnCard_TEMP;
 
     // Start is called before the first frame update
     void Start()
     {
         //camera = GetComponent<Camera>();
 
-        leftTurnCard_TEMP = GameObject.Find("LeftTurn_Card");
-        leftTurnCard_TEMP.SetActive(false);
+        GameObject leftTurnCard = GameObject.Find("LeftTurn_Card");
+        leftTurnCard.SetActive(false);
+
+        cards.Add(leftTurnCard);
+
+        GameObject rightTurnCard = GameObject.Find("RightTurn_Card");
+        rightTurnCard.SetActive(false);
+
+        cards.Add(rightTurnCard);
 
         playerController = player.GetComponent<PlayerController>();
         aiController = ai.GetComponent<AIController>();
 
-        for (int i = 0; i < cards.Count; i++)
-        {
-            Debug.Log(cards[i].name);
-            turnDict[cards[i].name] = cards[i].GetComponent<TurnCard>();
-        }
+        //for (int i = 0; i < cards.Count; i++)
+        //{
+        //    Debug.Log(cards[i].name);
+        //    turnDict[cards[i].name] = cards[i].GetComponent<TurnCard>();
+        //}
     }
 
     // Update is called once per frame
@@ -52,22 +59,22 @@ public class GameManager : MonoBehaviour
         //slow down time i.e. 'pause'
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            leftTurnCard_TEMP.SetActive(true);
             //set to 0 for true pause
             Time.timeScale = 0.2f; 
             for(int i = 0; i < cards.Count; i++)
             {
-                shownCards.Add(Object.Instantiate(cards[i], new Vector3(-5 + i * 5,-3,0), Quaternion.identity));
+                cards[i].SetActive(true);
+                //shownCards.Add(Object.Instantiate(cards[i], new Vector3(-5 + i * 5,-3,0), Quaternion.identity));
             }
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            leftTurnCard_TEMP.SetActive(false);
-            for (int i = 0; i < shownCards.Count; i++)
+            for (int i = 0; i < cards.Count; i++)
             {
-                Object.Destroy(shownCards[i]);
+                cards[i].SetActive(true);
+                //Object.Destroy(shownCards[i]);
             }
-            shownCards.Clear();
+            //shownCards.Clear();
             Time.timeScale = 1;
         }
 
