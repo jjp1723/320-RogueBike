43d799566cf9726531ae9d6033112a433b2d105d
Cassiar <cdb7951@rit.edu>
2021-12-07 14:31:23 -0500

    fastest time no longer shows 0

--- RogueBike/Assets/Scripts/GameManager.cs
+++ RogueBike/Assets/Scripts/GameManager.cs
@@ -74,6 +74,7 @@ public class GameManager : MonoBehaviour
         //}
 
         time = -3.0f;
+
     }
 
     // Update is called once per frame
--- RogueBike/Assets/Scripts/HighScoreManager.cs
+++ RogueBike/Assets/Scripts/HighScoreManager.cs
@@ -19,6 +19,8 @@ public class HighScoreManager : MonoBehaviour
     [SerializeField]
     private TMP_Text fastestTimeText;
 
+    private bool firstGame = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,19 +28,28 @@ public class HighScoreManager : MonoBehaviour
         playerTime = PlayerPrefs.GetFloat("PlayerTime");
         aiTime = PlayerPrefs.GetFloat("AITime");
 
-    }
+        firstGame = PlayerPrefs.GetInt("firstGame") == 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (playerTime < prevHS)
+        if (firstGame)
+        {
+            PlayerPrefs.SetInt("firstGame", 1);
+            prevHS = playerTime;
+            PlayerPrefs.SetFloat("highscore", prevHS);
+        }
+        else if (playerTime < prevHS)
         {
             prevHS = playerTime;
+            PlayerPrefs.SetFloat("highscore", prevHS);
         }
 
         aiTimeText.text = "AI Time: " + string.Format("{0,2:00}:{1,2:00.00}", (int)aiTime / 60, aiTime % 60);
         playerTimeText.text = "Player Time: " + string.Format("{0,2:00}:{1,2:00.00}", (int)playerTime / 60, playerTime % 60);
         fastestTimeText.text = "Fastest Time: " + string.Format("{0,2:00}:{1,2:00.00}", (int)prevHS / 60, prevHS % 60);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 }
